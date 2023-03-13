91b89442494e10d5eb16c1dba1b5ffa54c105ba2
juliangainar <gainar.ionut@gmail.com>
2023-03-13 19:18:26 +0100

    Added : Classe Item

---
+++ src/modele/Item.java
@@ -0,0 +1,50 @@
+package modele;
+// import String ???
+
+
+/**
+ * Classe gerant les items du jeu
+ * @author Iulian GAINAR
+ *
+ */
+public class Item {
+	// Parametres
+    private String name;
+    private String description;
+    private int price;
+    
+    // Constructeurs
+    public Item(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+    
+    // Accesseurs
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public int getPrice() {
+        return price;
+    }
+    
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+    // Methodes
+}
+
