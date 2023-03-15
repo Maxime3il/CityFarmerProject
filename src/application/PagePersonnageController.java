9fb89d62f5661b6292f07adcf16223827c2f807c
GarnierMarius <garnierm@3il.fr>
2023-03-15 23:33:49 +0100

    Interface graphique de la page personnage terminée

--- src/application/PagePersonnageController.java
+++ src/application/PagePersonnageController.java
@@ -25,28 +25,28 @@ public class PagePersonnageController {
 	private ComboBox<String> myComboBox;
 	@FXML
 	public void initialize() {
-	    myComboBox.getItems().addAll("Homme", "Femme", "Autre");
-	    myComboBox.setValue("Homme");
+		myComboBox.getItems().addAll("Homme", "Femme", "Autre");
+		myComboBox.setValue("Homme");
 
-	    personnages = personnagesHommes;
-	    afficherImage();
+		personnages = personnagesHommes;
+		afficherImage();
 
-	    myComboBox.setOnAction((ActionEvent event) -> {
-	        String selectedValue = myComboBox.getValue();
-	        switch (selectedValue) {
-	            case "Homme":
-	                personnages = personnagesHommes;
-	                break;
-	            case "Femme":
-	                personnages = personnagesFemmes;
-	                break;
-	            case "Autre":
-	            	default :
-	                personnages = personnagesAutre;
-	                break;
-	        }
-	        afficherImage();
-	    });
+		myComboBox.setOnAction((ActionEvent event) -> {
+			indice = 0;
+			String selectedValue = myComboBox.getValue();
+			switch (selectedValue) {
+			case "Homme":
+				personnages = personnagesHommes;
+				break;
+			case "Femme":
+				personnages = personnagesFemmes;
+				break;
+			case "Autre":
+				personnages = personnagesAutre;
+				break;
+			}
+			afficherImage();
+		});
 	}
 
 	private String[] personnages;
@@ -56,47 +56,47 @@ public class PagePersonnageController {
 
 	@FXML
 	private ImageView MyImageView;
-	
-	@FXML
-    private ImageView nextButton;
 
 	@FXML
-    private ImageView previousButton;
+	private ImageView nextButton;
+
+	@FXML
+	private ImageView previousButton;
 
 	private int indice = 0;
 
 	@FXML
 	public void handleNextButton(MouseEvent event) {
-	    indice++;
-	    if (indice >= personnages.length) {
-	        indice = 0;
-	    }
-	    afficherImage();
+		indice++;
+		if (indice >= personnages.length) {
+			indice = 0;
+		}
+		afficherImage();
 	}
 
 	@FXML
 	public void handlePreviousButton(MouseEvent event) {
-	    indice--;
-	    if (indice < 0) {
-	        indice = personnages.length - 1;
-	    }
-	    afficherImage();
+		indice--;
+		if (indice < 0) {
+			indice = personnages.length - 1;
+		}
+		afficherImage();
 	}
-
 
 	private void afficherImage() {
-	    Image image = new Image(getClass().getResourceAsStream(personnages[indice]));
-	    MyImageView.setImage(image);
+		Image image = new Image(getClass().getResourceAsStream(personnages[indice]));
+		MyImageView.setImage(image);
 	}
-	
+
 	@FXML
-    private Button validatePerso;
+	private Button validatePerso;
+
 	@FXML
-    public void valider(ActionEvent event) {
+	public void valider(ActionEvent event) {
 		System.out.println();
 		Stage stage = (Stage) closeButton.getScene().getWindow();
 		stage.close();
-    }
+	}
 
 
 
